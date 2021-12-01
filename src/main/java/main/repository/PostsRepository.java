0d579dc22cb63e08090933c0dd7db4fbd79c5a88
alexei <alexei.muraschkin@mail.ru>
2021-12-01 08:49:51 +0500

    second step fix

--- src/main/java/main/repository/PostsRepository.java
+++ src/main/java/main/repository/PostsRepository.java
@@ -30,17 +30,4 @@ public interface PostsRepository extends JpaRepository<Post, Integer> {
     @Query(nativeQuery = true, value = "SELECT * FROM posts LEFT JOIN post_comments pc on posts.id = pc.post_id " +
             "WHERE posts.is_active = 1 AND posts.moderation_status = 'ACCEPTED' AND posts.time <= now() GROUP BY posts.id ORDER BY COUNT(pc.post_id) DESC")
     public List<Post> findAllByPostCommentCount(Pageable pageable);
-
-    @Query(nativeQuery = true, value = "WITH tag_temp AS(" +
-            "SELECT COUNT(tag_id) AS tag_count FROM posts " +
-            "JOIN tag2post t2p on posts.id = t2p.post_id " +
-            "JOIN tags t on t.id = t2p.tag_id WHERE is_active = 1 AND moderation_status = 'ACCEPTED' AND time <= NOW() AND t.id = :id), " +
-            "post_temp AS (" +
-            "SELECT COUNT(*) AS post_count FROM posts WHERE is_active = 1 AND moderation_status = 'ACCEPTED' AND time <= now()), " +
-            "max_count_post_by_tag AS (" +
-            "SELECT MAX(count) AS max_count FROM (SELECT COUNT(tag_id) AS count FROM posts " +
-            "JOIN tag2post p on posts.id = p.post_id " +
-            "JOIN tags t2 on t2.id = p.tag_id WHERE is_active = 1 AND moderation_status = 'ACCEPTED' AND time <= now() GROUP BY t2.id) AS temp)" +
-            "SELECT (1 / (max_count / post_count)) * (tag_count / post_count) FROM tag_temp JOIN post_temp JOIN max_count_post_by_tag")
-    public double getTagWeight(@Param("id") int id);
 }
--- src/main/java/main/service/TagsServiceImpl.java
+++ src/main/java/main/service/TagsServiceImpl.java
@@ -1,28 +1,24 @@
 package main.service;
 
 import main.api.response.TagsResponse;
-import main.mapper.TagMapper;
 import main.repository.TagsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.stream.Collectors;
 
 @Service
 public class TagsServiceImpl implements TagsService {
-    private final TagMapper mapper;
     private final TagsRepository tagsRepository;
 
     @Autowired
-    public TagsServiceImpl(TagMapper mapper, TagsRepository tagsRepository) {
-        this.mapper = mapper;
+    public TagsServiceImpl(TagsRepository tagsRepository) {
         this.tagsRepository = tagsRepository;
     }
 
     @Override
     public TagsResponse getTags() {
         TagsResponse tagResponse = new TagsResponse();
-        tagResponse.setTags(tagsRepository.findAll().stream().map(mapper::toDTO).collect(Collectors.toSet()));
+        tagResponse.setTags(tagsRepository.getTags());
         return tagResponse;
     }
 }
