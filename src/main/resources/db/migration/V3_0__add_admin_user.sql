2ddad66baae7ad464bfbc2d0b476ce1cc38fee36
alekseyNyagan <alexei.muraschkin@mail.ru>
2022-04-15 15:18:34 +0500

    admin user migration, start.sh, maven-surefire-plugin, Dockerfile, docker-compose.yml added and test data deleted

---
+++ src/main/resources/db/migration/V3_0__add_admin_user.sql
@@ -0,0 +1,1 @@
+insert into users (id, is_moderator, reg_time, name, email, password, code, photo) values (1, 1, '2021-09-18 22:01:33', 'admin', 'admin@admin.ru', '$2a$12$7GZ5zptWYUt364zNLKaT2ut3XAU29J3hmxM3avVs/bKPjN0fjYECy', null, null);
